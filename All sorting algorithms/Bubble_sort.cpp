4663ce8862aebd87bab35e5475f71a866a967a62
Ketan Chauhan <98975157+Ketan9548@users.noreply.github.com>
2024-11-14 04:37:44 +0000

    done

--- All sorting algorithms/Bubble_sort.cpp
+++ All sorting algorithms/Bubble_sort.cpp
@@ -1,11 +1,41 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+// int main()
+// {
+//     vector<int> v = {2, 1, 4, 5, 12, 992, 8, 6};
+//     // vector<int> v = {3, 5, 7, 8, 9, 10, 13, 19};
+//     int n = v.size();
+//     int counter = 1;
+//     while (counter < n)
+//     {
+//         for (int i = 0; i < n - counter; i++)
+//         {
+//             if (v[i] > v[i + 1])
+//             {
+//                 swap(v[i], v[i + 1]);
+//             }
+//         }
+//         counter++;
+//     }
+//     for (auto &&i : v)
+//     {
+//         cout << i << " ";
+//     }
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int main()
 {
-    // vector<int> v = {2, 1, 4, 5, 12, 992, 8, 6};
-    vector<int> v = {3, 5, 7, 8, 9, 10, 13, 19};
+    vector<int> v = {91, 43, 14, 4, 3, 2, 1, 191, 21};
     int n = v.size();
     int counter = 1;
     while (counter < n)
@@ -19,9 +49,10 @@ int main()
         }
         counter++;
     }
+
     for (auto &&i : v)
     {
-        cout << i << " ";
+        cout << "value is: " << i << endl;
     }
 
     return 0;
